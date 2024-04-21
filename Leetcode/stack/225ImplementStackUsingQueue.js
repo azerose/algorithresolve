7280d139f4b6e33f028469f58d0daf474a4acae6
azerose <woojjang8671@naver.com>
2024-04-21 18:39:25 +0900

    [Add] add resolve that using queue make Stack

--- Leetcode/stack/225ImplementStackUsingQueue.js
+++ Leetcode/stack/225ImplementStackUsingQueue.js
@@ -1,54 +1,48 @@
-var MyStack = function () {
-  this.inQueue = [];
-  this.outQueue = [];
+var MyQueue = function () {
+  this.preStack = [];
+  this.postStack = [];
 };
 
 /**
- * Push element x onto stack.
  * @param {number} x
  * @return {void}
  */
-MyStack.prototype.push = function (x) {
-  this.inQueue.push(x);
+MyQueue.prototype.push = function (x) {
+  while (this.preStack.length > 0) {
+    this.postStack.push(this.preStack.pop());
+  }
+  this.preStack.push(x);
+  while (this.postStack.length > 0) {
+    this.preStack.push(this.postStack.pop());
+  }
 };
 
 /**
- * Removes the element on top of the stack and returns that element.
  * @return {number}
  */
-MyStack.prototype.pop = function () {
-  while (this.inQueue.length > 1) {
-    this.outQueue.push(this.inQueue.shift());
-  }
-
-  const lastItem = this.inQueue.shift();
-
-  [this.inQueue, this.outQueue] = [this.outQueue, this.inQueue];
-
-  return lastItem;
+MyQueue.prototype.pop = function () {
+  return this.preStack.pop();
 };
 
 /**
- * Get the top element.
  * @return {number}
  */
-MyStack.prototype.top = function () {
-  while (this.inQueue.length > 1) {
-    this.outQueue.push(this.inQueue.shift());
-  }
-  // peak
-  const lastItem = this.inQueue[0];
-
-  this.outQueue.push(this.inQueue.shift());
-  [this.inQueue, this.outQueue] = [this.outQueue, this.inQueue];
-
-  return lastItem;
+MyQueue.prototype.peek = function () {
+  return this.preStack[this.preStack.length - 1];
 };
 
 /**
- * Returns whether the stack is empty.
  * @return {boolean}
  */
-MyStack.prototype.empty = function () {
-  return this.inQueue.length === 0;
+MyQueue.prototype.empty = function () {
+  return this.preStack.length === 0;
 };
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
