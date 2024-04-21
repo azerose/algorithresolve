762355ac05cd7e7660939b85aeeb6761796fd308
azerose <woojjang8671@naver.com>
2024-04-21 19:17:10 +0900

    [Add] add description

--- Leetcode/stack/225ImplementStackUsingQueue.js
+++ Leetcode/stack/225ImplementStackUsingQueue.js
@@ -10,11 +10,13 @@ var MyQueue = function () {
 MyQueue.prototype.push = function (x) {
   while (this.preStack.length > 0) {
     this.postStack.push(this.preStack.pop());
-  }
-  this.preStack.push(x);
+  } // preStack의 크기가 0보다 크다면 postStack에 저장해서 preStack을 빈배열로 초기화
+  this.preStack.push(x); // 길이가 0일때 preStack에 저장해서 나중에 들어온값이 이전에 들어온값 뒤에 위치할수있게함
   while (this.postStack.length > 0) {
     this.preStack.push(this.postStack.pop());
-  }
+  } // 이전에 저장했던 postStack에서 값을 다시 push를 통해서 가장 끝에 위치시킴
+
+  // 먼저 담긴 값이 가장 뒤에 위치해있도록 조절
 };
 
 /**
